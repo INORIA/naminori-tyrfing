0e70afb1fbde1aac3b741335f6b88f5a4d90212f
INORIA <inoria@gfb.io>
2017-11-14 01:58:54 +0900

    Add Lancer

---
+++ Assets/Overload/Scripts/Enemies/ThrowLance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Overland.Scripts.Enemies
+{
+    public class ThrowLance : MonoBehaviour
+    {
+        public GameObject LancePrefab;
+
+        // Use this for initialization
+        void Start()
+        {
+            Fire();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void Fire()
+        {
+            var lance = Instantiate(LancePrefab, transform.position, Quaternion.identity);
+            lance.GetComponent<Rigidbody2D>().AddForce(new Vector2(-200, -200));
+            Destroy(lance, 2f);
+        }
+    }
+}
